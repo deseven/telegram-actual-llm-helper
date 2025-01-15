38e692bde3825b3b2186033a7ed632c3f478a152
deseven <git@d7.wtf>
2025-01-15 21:24:22 +0000

    +

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { exitOnError } = require('winston');
 const {
     logger, config, helpers,
     convertCurrency, prettyjson,
@@ -12,9 +13,14 @@ let Actual = InitActual();
 let Bot = InitBot();
 LaunchBot(Bot);
 
-App.listen(config.PORT, () => {
-    logger.info(`Server is running on port ${config.PORT}`);
-});
+try {
+    App.listen(config.PORT, () => {
+        logger.info(`Susccessfully started server on port ${config.PORT}.`);
+    });
+} catch (err) {
+    logger.error(`Failed to start server: ${err}`);
+    process.exit(1);
+}
 
 // -- Unified Message Handler --
 Bot.on('message', async (ctx) => {
